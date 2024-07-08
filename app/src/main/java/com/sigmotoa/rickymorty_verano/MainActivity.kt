2460074f7a614fb90eec4a97d4daecc2754df07b
sigmotoa <si.galvismotoa@gmail.com>
2024-07-08 09:28:36 -0500

    Compose 2, parky in a lazyColumn

--- app/src/main/java/com/sigmotoa/rickymorty_verano/MainActivity.kt
+++ app/src/main/java/com/sigmotoa/rickymorty_verano/MainActivity.kt
@@ -1,29 +1,60 @@
 package com.sigmotoa.rickymorty_verano
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sigmotoa.rickymorty_verano.ui.SCard
 import com.sigmotoa.rickymorty_verano.ui.theme.RickYMorty_VeranoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             RickYMorty_VeranoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                   // SBox("Hola desde una funcion")
+                    //SBox("hola otra vez")
+                    //SBox("Hola, me estoy robando la pantalla")
+                    //SColumn()
                     Greeting(
-                        name = "Android",
+                       name = "",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    LazyColumn {
+                        item { SCard() }
+                        item { Spacer(modifier = Modifier.height(48.dp)) }
+                        item { SCard() }
+                        item { SBox("Sigmotoa") }
+                        item { SCard() }
+                        item { SCard() }
+                        item { SCard() }
+                        item { SCard() }
+                        item { SCard() }
+                    }
+
+                    //SCard()
+
+
                 }
             }
         }
@@ -31,9 +62,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun SBox(texto:String) {
+    Box (modifier = Modifier.fillMaxWidth().background(Color.Yellow)){
+        Text(text = texto)
+       // Text(text="Estoy en un Box")
+    }
+}
+
+@Composable
+fun SColumn(modifier: Modifier = Modifier) {
+    Column (modifier = Modifier.fillMaxSize()){
+        Text(text = "Holas, estoy en una columna")
+        Text(text = "Holas, estoy debajo" , fontSize = 24.sp)
+        SBox("Estoy en una columna pero soy un box")
+        Text(text = "Estoy debajo de un box")
+        SBox("Estoy en una columna pero soy un box")
+
+
+    }
+}
+
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "",
         modifier = modifier
     )
 }
@@ -47,9 +100,10 @@ fun GreetingPreviewSigmotoa() {
 }
 
 @Preview(
-    device = "id:wearos_large_round", showSystemUi = true, showBackground = true,
+    device = "id:wearos_square", showSystemUi = true, showBackground = true,
     backgroundColor = 0xFF4E3CE2,
-    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_WATCH
+    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_WATCH,
+
 )
 @Composable
 fun PreviewKarol() {
@@ -59,7 +113,8 @@ fun PreviewKarol() {
 @Preview(
     device = "id:wearos_large_round", showSystemUi = true, showBackground = true,
     backgroundColor = 0xFF4E3CE2,
-    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_WATCH
+    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_WATCH,
+
 )
 @Composable
 fun PreviewJesus() {
---
+++ app/src/main/java/com/sigmotoa/rickymorty_verano/ui/RyMCard.kt
@@ -0,0 +1,56 @@
+package com.sigmotoa.rickymorty_verano.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sigmotoa.rickymorty_verano.R
+
+/**
+ *
+ * Created by sigmotoa on 8/07/24.
+ * @author sigmotoa
+ *
+ * www.sigmotoa.com
+ */
+
+@Composable
+fun SCard(modifier: Modifier = Modifier) {
+    ElevatedCard(
+        colors = CardDefaults.cardColors(containerColor = Color.Magenta),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        modifier = Modifier
+            .width(200.dp)
+            .height(240.dp)
+    ) {
+        Text(
+            "Soy sigmotoa en una card",
+            fontStyle = FontStyle.Italic,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Image(painterResource(R.drawable.parky_001), contentDescription = "Mi amigo")
+        Text("Soy el otro texto", textAlign = TextAlign.Center)
+    }
+}
+
+
+@Preview(showSystemUi = true, showBackground = true, backgroundColor = 0xFFF3EBEB)
+@Composable
+fun PrevCard(modifier: Modifier = Modifier) {
+    SCard()
+}
